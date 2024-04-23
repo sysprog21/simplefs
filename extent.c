884e6403a5186697457bf0e1209f09d652374319
apfeet <ottaviomalatacca030907@gmail.com>
2024-04-23 18:20:16 +0200

    made the algo more eff

--- extent.c
+++ extent.c
@@ -1,23 +1,40 @@
 #include <linux/fs.h>
 #include <linux/kernel.h>
+#include <linux/sort.h>
 
 #include "simplefs.h"
 
-/* Search for the extent containing the target block.
+/* Compare function for sort */
+int cmp(const void *a, const void *b)
+{
+    struct simplefs_extent *extentA = (struct simplefs_extent *)a;
+    struct simplefs_extent *extentB = (struct simplefs_extent *)b;
+    return extentA->ee_block - extentB->ee_block;
+}
+
+/* Binary Search for the extent containing the target block.
  * Returns the first unused file index if not found.
  * Returns -1 if the target block is out of range.
- * TODO: Implement binary search for efficiency.
  */
 uint32_t simplefs_ext_search(struct simplefs_file_ei_block *index,
                              uint32_t iblock)
 {
-    uint32_t i;
-    for (i = 0; i < SIMPLEFS_MAX_EXTENTS; i++) {
-        uint32_t block = index->extents[i].ee_block;
-        uint32_t len = index->extents[i].ee_len;
-        if (index->extents[i].ee_start == 0 ||
+    /* Sort the extents by ee_block */
+    sort(index->extents, SIMPLEFS_MAX_EXTENTS, sizeof(struct simplefs_extent), cmp, NULL);
+
+    uint32_t start = 0;
+    uint32_t end = SIMPLEFS_MAX_EXTENTS - 1;
+    while (start <= end) {
+        uint32_t mid = start + (end - start) / 2;
+        uint32_t block = index->extents[mid].ee_block;
+        uint32_t len = index->extents[mid].ee_len;
+        if (index->extents[mid].ee_start == 0 ||
             (iblock >= block && iblock < block + len))
-            return i;
+            return mid;
+        if (index->extents[mid].ee_block < iblock)
+            start = mid + 1;
+        else
+            end = mid - 1;
     }
 
     return -1;
